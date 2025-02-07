fab571d3b638fb7420b4cd365094fb61f30172e8
Cerra Cheraka Mouna <Mouna.Cerra-Cheraka@chuv.ch>
2025-02-07 16:32:16 +0100

    config paths changed again

--- src/Configuration.py
+++ src/Configuration.py
@@ -4,14 +4,14 @@ import yaml
 # USER CONFIG
 
 def get_user_bids_path():
-    with open('user_config.yaml', 'r') as file:
+    with open('src/user_config.yaml', 'r') as file:
         config = yaml.safe_load(file)
     file.close()
     return config['bids_path']
 
 
 def get_extraction_successful():
-    with open('user_config.yaml', 'r') as file:
+    with open('src/user_config.yaml', 'r') as file:
         config = yaml.safe_load(file)
     file.close()
     return config['extraction_successful']
@@ -20,29 +20,29 @@ def get_extraction_successful():
 def update_user_path(value):
     if(not value.endswith('/')):
         value = value + '/'
-    with open('user_config.yaml', 'r') as file:
+    with open('src/user_config.yaml', 'r') as file:
         config = yaml.safe_load(file)
   
     config['bids_path'] = value
 
-    with open('user_config.yaml', 'w') as file:
+    with open('src/user_config.yaml', 'w') as file:
         yaml.dump(config, file)
 
     file.close()
     
 def update_extraction_value(value):
-    with open('user_config.yaml', 'r') as file:
+    with open('src/user_config.yaml', 'r') as file:
         config = yaml.safe_load(file)
     
     config['extraction_successful'] = value
 
-    with open('user_config.yaml', 'w') as file:
+    with open('src/user_config.yaml', 'w') as file:
         yaml.dump(config, file)
 
     file.close()
     
 def get_output_file_names():
-    with open('config.yaml', 'r') as file:
+    with open('src/config.yaml', 'r') as file:
         config = yaml.safe_load(file)
     
     file.close()
